1d6c293ca632857571160484d8d2fe5b08678364
schktjm <schktjm@gmail.com>
2022-11-27 20:56:15 +0900

    :+1: esm を実行できるように

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
-const s: string = 'hogehoge'
+#!/usr/bin/env node
+import meow from 'meow';
+
+const s: string = 'hogehoge';
 
 console.log(s)
+
+const cli = meow(`
+    Usage
+        $ genbacat
+`, {
+    importMeta: import.meta,
+})
+
+console.log(cli.input)
